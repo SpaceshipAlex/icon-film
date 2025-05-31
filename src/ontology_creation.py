8bda92ed4d7bd1c9f1339f3c5def76303d782a14
SpaceshipAlex <spaceshipalex3@gmail.com>
2025-05-31 18:46:57 +0200

    Setup iniziale, creazione ontologia, file test API

---
+++ src/ontology_creation.py
@@ -0,0 +1,101 @@
+from owlready2 import *
+import os
+
+ONTO_FILENAME = "movie_rating_ontology.owl"
+ONTO_PATH = os.path.join("ontology", ONTO_FILENAME) # non è ../ontology perché la working directory di Python è già nella cartella superiore
+ONTO_IRI = "http://test.org/" + ONTO_FILENAME[:-4] ### rimuovo .owl nell'IRI
+
+if os.path.exists(ONTO_PATH):
+    try:
+        os.remove(ONTO_PATH)
+        print("Rimossa ontologia esistente")
+    except OSError as e:
+        print(f"Errore nella rimozione dell'ontologia corrente: {e}")
+
+onto = get_ontology(ONTO_IRI)
+
+with onto: # per evitare di scrivere (namespace = onto) in ogni classe, si scrive pass
+    # CLASSI
+    class Film(Thing): pass
+    class Person(Thing): pass
+    class Director(Person): pass # sotto-classe di Person
+    class Actor(Person): pass
+    class Studio(Thing): pass
+    class Award(Thing): pass
+    class Genre(Thing): pass
+
+    # PROPRIETA'
+    class hasDirector(ObjectProperty):
+        domain = [Film]
+        range = [Director]
+    class hasActor(ObjectProperty):
+        domain = [Film]
+        range = [Actor]
+    class producedByStudio(ObjectProperty):
+        domain = [Film]
+        range = [Studio]
+    class distributedByStudio(ObjectProperty):
+        domain = [Film]
+        range = [Studio]
+    class wonAward(ObjectProperty):
+        domain = [Person, Film]
+        range = [Award]
+    class hasGenre(ObjectProperty):
+        domain = [Film]
+        range = [Genre]
+
+    class filmTitle(FunctionalProperty):
+        domain = [Film]
+        range = [str]
+    class tmdbRating(FunctionalProperty):
+        domain = [Film]
+        range = [float]
+    class budget(FunctionalProperty):
+        domain = [Film]
+        range = [int]
+    class revenue(FunctionalProperty):
+        domain = [Film]
+        range = [int]
+    class releaseDate(FunctionalProperty):
+        domain = [Film]
+        range = [datetime.date]
+    class runtime(FunctionalProperty):
+        domain = [Film]
+        range = [int]
+
+    class personName(FunctionalProperty):
+        domain = [Person]
+        range = [str]
+    class birthDate(FunctionalProperty):
+        domain = [Person]
+        range = [datetime.date]
+    class careerStartDate(FunctionalProperty):
+        domain = [Person]
+        range = [datetime.date]
+    class nationality(FunctionalProperty):
+        domain = [Person]
+        range = [str]
+    class nAwards(FunctionalProperty):
+        domain = [Person]
+        range = [int]
+    
+    class avgRatingOfDirectedFilms(FunctionalProperty):
+        domain = [Director]
+        range = [float]
+    class nFilmsDirected(FunctionalProperty):
+        domain = [Director]
+        range = [int]
+    class nFilmsActed(FunctionalProperty):
+        domain = [Actor]
+        range = [int]
+
+    class awardName(FunctionalProperty):
+        domain = [Award]
+        range = [str]
+    class awardYear(FunctionalProperty):
+        domain = [Award]
+        range = [int]
+    
+onto.save(file = ONTO_PATH, format = "rdfxml")
+print("Struttura dell'ontologia definita e salvata")
+
---
+++ src/test.py
@@ -0,0 +1,44 @@
+import requests
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+import config
+
+### API TMDB
+
+headers = { # Da documentazione API
+    "accept": "application/json",
+    "Authorization": "Bearer  eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDRmNjViYzc2MjgwYjdjNTgwOWU3ODMyZGRjMzRkOCIsIm5iZiI6MTY0NTEwOTI2NS4zNjIsInN1YiI6IjYyMGU2MDExZjc5NGFkMDA2YWMwMTRmNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ln5tzSqLmjK2wx5d3132fAS9S5AEUlmiX_w38LbpEfI"
+}
+
+try:
+    response = requests.get(f"{config.TMDB_BASE_URL}/movie/812/external_ids", headers = headers) # Chiedo gli ID esterni di un film (tra cui ID Wikidata)
+    response.raise_for_status(); # Solleva eccezione in caso di errori HTTP
+    print(f"TMDB:\n{response.text}\n\n")
+except requests.exceptions.RequestException as e:
+    print(f"Errore API TMDb: {e}")
+
+
+### API WIKIDATA
+
+sparql_endpoint = "https://query.wikidata.org/sparql"
+sparql = SPARQLWrapper(sparql_endpoint)
+sparql.setReturnFormat(JSON)
+sparql.addCustomHttpHeader("User-Agent", "KnowledgeEngineeringUniversityProject/1.0 (a.duca1@studenti.uniba.it)") ### Buona pratica
+
+# Nella query SPARQL sostituire tutte le { con {{ altrimenti Python restituisce un errore
+# Query di esempio, cerca tutti gli instance of (P31) house cat (Q146)
+sparql.setQuery(f""" 
+    SELECT ?item ?itemLabel WHERE
+    {{
+     ?item wdt:P31 wd:Q146. 
+     SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],mul,en". }}
+    }}
+    LIMIT 10
+    """
+)
+
+try:
+    results = sparql.query().convert()
+    print(f"Wikidata:\n{results["results"]["bindings"]}")
+except Exception as e:
+    print(f"Errore query Wikidata: {e}")
